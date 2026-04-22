e5fba127388343c43504d16ba867dfacf2fc8e5a
smd <88483238+x8vxv8x@users.noreply.github.com>
2026-04-22 14:31:56 +0800

    Update DropHandler.java

--- src/main/java/lellson/foodexpansion/DropHandler.java
+++ src/main/java/lellson/foodexpansion/DropHandler.java
@@ -1,72 +1,59 @@
 package lellson.foodexpansion;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityPolarBear;
-import net.minecraft.entity.passive.EntityBat;
-import net.minecraft.entity.passive.EntityHorse;
-import net.minecraft.entity.passive.EntityLlama;
-import net.minecraft.entity.passive.EntityOcelot;
-import net.minecraft.entity.passive.EntityParrot;
-import net.minecraft.entity.passive.EntitySquid;
-import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.entity.passive.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DropHandler {
-	
-	public static final List<Drop> DROP_LIST = new ArrayList<>();
-	
-	public DropHandler() {
-		
-		addDrop(EntitySquid.class, FoodItems.itemSquid, FoodItems.itemCookedSquid, 2);
-		addDrop(EntityHorse.class, FoodItems.itemHorseMeat, FoodItems.itemCookedHorseMeat, 3);
-		addDrop(EntityBat.class, FoodItems.itemBatWing, FoodItems.itemCookedBatWing, 1);
-		addDrop(EntityWolf.class, FoodItems.itemWolfMeat, FoodItems.itemCookedWolfMeat, 2);
-		addDrop(EntityOcelot.class, FoodItems.itemOcelotMeat, FoodItems.itemCookedOcelotMeat, 1);
-		addDrop(EntityParrot.class, FoodItems.itemParrotMeat, FoodItems.itemCookedParrotMeat, 1);
-		addDrop(EntityLlama.class, FoodItems.itemLlamaMeat, FoodItems.itemCookedLlamaMeat, 2);
-		addDrop(EntityPolarBear.class, FoodItems.itemPolarBearMeat, FoodItems.itemCookedPolarBearMeat, 3);
+
+	private static final Map<Class<? extends EntityLivingBase>, DropData> DROP_MAP = new HashMap<>();
+
+	static {
+		add(EntitySquid.class,FoodItems.itemSquid,FoodItems.itemCookedSquid,2);
+		add(EntityHorse.class,FoodItems.itemHorseMeat,FoodItems.itemCookedHorseMeat,3);
+		add(EntityBat.class,FoodItems.itemBatWing,FoodItems.itemCookedBatWing,1);
+		add(EntityWolf.class,FoodItems.itemWolfMeat,FoodItems.itemCookedWolfMeat,2);
+		add(EntityOcelot.class,FoodItems.itemOcelotMeat,FoodItems.itemCookedOcelotMeat,1);
+		add(EntityParrot.class,FoodItems.itemParrotMeat,FoodItems.itemCookedParrotMeat,1);
+		add(EntityLlama.class,FoodItems.itemLlamaMeat, FoodItems.itemCookedLlamaMeat,2);
+		add(EntityPolarBear.class,FoodItems.itemPolarBearMeat, FoodItems.itemCookedPolarBearMeat,3);
 	}
 
-	private void addDrop(Class entityClass, Item uncooked, Item cooked, int maxDropAmount) {
-		DROP_LIST.add(new Drop(entityClass, uncooked, cooked, maxDropAmount));
+	private static void add(Class<? extends EntityLivingBase> entityClass, Item raw, Item cooked, int maxAmount) {
+		DROP_MAP.put(entityClass, new DropData(raw, cooked, maxAmount));
 	}
 
 	@SubscribeEvent
 	public void onEntityDrop(LivingDropsEvent event) {
-		
-		for(Drop drop : DROP_LIST) 
-		{
-			drop.tryDrop(event.getEntityLiving());
+		EntityLivingBase entity = event.getEntityLiving();
+
+		if (entity.isChild()) return;
+
+		DropData data = DROP_MAP.get(entity.getClass());
+		if (data != null) {
+
+			Item dropItem = entity.isBurning() ? data.cooked : data.raw;
+			int amount = entity.world.rand.nextInt(data.maxAmount) + 1;
+			entity.entityDropItem(new ItemStack(dropItem, amount), 0.5F);
 		}
 	}
-	
-	public class Drop {
-		
-		public boolean cfgDisable;
-		public Class entityClass;
-		public Item uncooked, cooked;
-		public int maxDropAmount;
 
-		public Drop(Class entityClass, Item uncooked, Item cooked, int maxDropAmount) {
+	private static class DropData {
+		final Item raw;
+		final Item cooked;
+		final int maxAmount;
 
-			this.entityClass = entityClass;
-			this.uncooked = uncooked;
+		DropData(Item raw, Item cooked, int maxAmount) {
+			this.raw = raw;
 			this.cooked = cooked;
-			this.maxDropAmount = maxDropAmount;
-		}
-		
-		public void tryDrop(EntityLivingBase entity) {
-			
-			if (!cfgDisable && entityClass.isInstance(entity) && !entity.isChild())
-			{
-				entity.entityDropItem(new ItemStack(entity.isBurning() ? cooked : uncooked, entity.getEntityWorld().rand.nextInt(maxDropAmount)+1), 0.5F);
-			}
+			this.maxAmount = maxAmount;
 		}
 	}
 }
